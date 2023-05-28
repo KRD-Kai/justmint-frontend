06c69873c59363d21310a987de19c3318a827219
KRD <kaimorciniec@gmail.com>
2023-05-28 01:53:52 +0100

    Add mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,34 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 import "@biconomy/web3-auth/dist/src/style.css";
-import { Inter } from "@next/font/google";
-const inter = Inter({ subsets: ["latin"] });
+// import { Inter } from "@next/font/google";
+// const inter = Inter({ subsets: ["latin"] });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps) {
+	const { Component, pageProps } = props;
+
 	return (
-		<main className={inter.className}>
-			<Component {...pageProps} />
-		</main>
+		<>
+			<Head>
+				<title>EscrowDao</title>
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width"
+				/>
+			</Head>
+
+			<MantineProvider
+				withGlobalStyles
+				withNormalizeCSS
+				theme={{
+					/** Put your mantine theme override here */
+					colorScheme: "light",
+				}}
+			>
+				<Component {...pageProps} />
+			</MantineProvider>
+		</>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useAccount } from "../hooks/useAccount";
+import { Button } from "@mantine/core";
 
 function Index() {
 	const { smartAccount, loading, login, logout } = useAccount();
@@ -6,14 +7,14 @@ function Index() {
 	return (
 		<>
 			{!smartAccount && !loading && (
-				<button onClick={login}>Login</button>
+				<Button onClick={login}>Login</Button>
 			)}
 			{loading && <p>Loading account details...</p>}
 			{!!smartAccount && (
 				<div>
 					<h3>Smart account address:</h3>
 					<p>{smartAccount.address}</p>
-					<button onClick={logout}>Logout</button>
+					<Button onClick={logout}>Logout</Button>
 				</div>
 			)}
 		</>
